566660e1d2336189108664bbf2e955921c0829c8
Luca Rojas Massey <lucasrojas95@gmail.com>
2024-02-01 19:41:37 -0300

    Se agrega foto de perfil

--- src/components/pages/Hero.jsx
+++ src/components/pages/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import heroIcon from "/assets/images/hero.png";
+import profilePhoto from "/assets/images/profile.png";
 import { languageSelector } from "../redux/sliceLanguage";
 import { useSelector } from "react-redux";
 
@@ -13,10 +13,10 @@ const Hero = () => {
       id="home"
       className="min-h-screen flex py-10 md:flex-row flex-col items-center"
     >
-      <div className="flex-1 flex items-center justify-center h-full">
-        <img src={heroIcon} alt="" className="md:w-11/12 h-full object-cover" />
+      <div className="flex-1 flex items-center justify-center rounded-xl md:mt-0 mt-10 w-80 mt-20">
+        <img src={profilePhoto} alt="" className="border-8 border-sky-500 2sm:w-[75%] md:w-6/12 h-full object-cover rounded-xl" />
       </div>
-      <div className="flex-1">
+      <div className="flex-1 mt-5">
         <div className="md:text-left text-center">
           <h1 className="md:text-5xl text-2xl md:leading-normal leading-10 text-white font-bold">
             <span className="text-cyan-600 md:text-6xl text-5xl">
@@ -30,14 +30,14 @@ const Hero = () => {
           </h4>
           <a className="w-[9.5rem] btn-primary mt-8" href="#contact">{hero?.contact_me}</a>
           <div className="mt-8 text-3xl flex items-center md:justify-start justify-center gap-5">
-            {hero?.social_media?.map(({icon, link}) => (
+            {hero?.social_media?.map(({ icon, link }) => (
               <div
                 key={icon}
                 className="text-gray-600 hover:text-white cursor-pointer "
               >
-                  <a href={link} target="_blank">
-                    <ion-icon name={icon}></ion-icon>
-                  </a>
+                <a href={link} target="_blank">
+                  <ion-icon name={icon}></ion-icon>
+                </a>
               </div>
             ))}
           </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: "jit",
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
@@ -6,6 +7,19 @@ module.exports = {
       fontFamily: {
         Poppins: "Poppins",
       },
+    },
+    screens: {
+      '2sm': { max: "480px" },
+
+      sm: "640px",
+
+      md: "768px",
+
+      lg: "1024px",
+
+      xl: "1280px",
+
+      "2xl": "1536px",
     },
   },
   plugins: [],
